dfc5cc25376c845229e3b86d76c71a0d2f3a4c74
Alongkon Natphunwat <136414303+nine9031@users.noreply.github.com>
2025-06-23 13:10:30 +0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,36 +1,42 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Restaurant from '../components/Restaurant'
-
 const Home = () => {
-    return (
-        <div className='container mx-auto'>
-            <Navbar />
-            <div>
-                <h1 className='title justify-center text-3xl text-canter m-5 p-5'>
-                    Grab Rastaurant
-                </h1>
-                <label className="input">
-                    <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                        <g
-                            strokeLinejoin="round"
-                            strokeLinecap="round"
-                            strokeWidth="2.5"
-                            fill="none"
-                            stroke="currentColor"
-                        >
-                            <circle cx="11" cy="11" r="8"></circle>
-                            <path d="m21 21-4.3-4.3"></path>
-                        </g>
-                    </svg>
-                    <input type="search" className="grow" placeholder="Search" />
-                    <kbd className="kbd kbd-sm"></kbd>
-                    <kbd className="kbd kbd-sm"></kbd>
-                </label>
-                <Restaurant />
-            </div>
+  return (
+    <div className='container mx-auto '>
+      {
+        //Navigation Bar
+      }
+        <Navbar />
+      {
+        //Header
+      }
+        <div>
+            <h1 className='title justify-center text-3xl text-center m-5 p-5 '>Grab Restaurant</h1>    
         </div>
-    )
+      {
+        //Sreach Bar
+      }  
+        <div className='mb-5 flex justify-center item-center'>
+        <label className="input flex item-center gap-2 w-2xl">
+  <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <g
+      strokeLinejoin="round"
+      strokeLinecap="round"
+      strokeWidth="2.5"
+      fill="none"
+      stroke="currentColor"
+    >
+      <circle cx="11" cy="11" r="8"></circle>
+      <path d="m21 21-4.3-4.3"></path>
+    </g>
+  </svg>
+  <input type="search" required placeholder="Search" />
+</label>
+    </div >
+        <Restaurant />
+    </div>
+  )
 }
 
 export default Home
